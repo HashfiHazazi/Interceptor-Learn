9770a05d2741c0538a3d628e607020b2a8e8233c
Hashfi Hazazi <muhamadhazazi52@gmail.com>
2024-03-04 09:32:29 +0700

    Solve initial screen route

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         fontFamily: 'Quicksand',
       ),
       onGenerateRoute: route.onRoute,
-      initialRoute: RouteNames.bottomNav,
+      initialRoute: RouteNames.login,
       debugShowCheckedModeBanner: false,
     );
   }
